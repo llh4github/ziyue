223e69b16c9ff8b50420c0676e859cb0b5e52b17
llh <forever2y_y@163.com>
2021-03-21 14:13:25 +0800

    columnlist 组件基本设计

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 const app = createApp(App)
-
-app.use(ElementPlus)
 
 app.mount('#app')
